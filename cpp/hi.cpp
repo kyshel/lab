481cadf6a0395d715abbc7bc3ab43ccb22629906
root <1069072177@qq.com>
2017-06-05 19:21:56 +0800

    first commit

---
+++ cpp/hi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "hi, this is step one project\n";
+
+
+
+	int *p;
+	int a;
+
+	a=10;
+	p=&a;
+
+	cout << p << endl;
+	cout << *p << endl;
+	cout << &p << endl;
+
+
+	cout << "100";
+
+
+	return 0;
+
+}
---
+++ go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+mkdir -p out
+cd out
+
+cmake ../cpp/
+make
+
+cd $SCRIPTPATH
+
+
+
